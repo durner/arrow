851eaa6c569afc2a2e7394913ec2f0bab7a10a54
Dominik Durner <durner@zoho.com>
2020-04-03 18:48:07 -0700

    ARROW-8334: [C++] [Gandiva] Missing DATE32 in Gandiva

--- cpp/src/gandiva/llvm_types.cc
+++ cpp/src/gandiva/llvm_types.cc
@@ -35,6 +35,7 @@ LLVMTypes::LLVMTypes(llvm::LLVMContext& context) : context_(context) {
       {arrow::Type::type::FLOAT, float_type()},
       {arrow::Type::type::DOUBLE, double_type()},
       {arrow::Type::type::DATE64, i64_type()},
+      {arrow::Type::type::DATE32, i32_type()},
       {arrow::Type::type::TIME32, i32_type()},
       {arrow::Type::type::TIME64, i64_type()},
       {arrow::Type::type::TIMESTAMP, i64_type()},
